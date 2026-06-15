232cc18d778bda4440355a5905b563589ed42533
AC3-CORP <ayeman2908@gmail.com>
2026-06-14 22:05:44 -0600

    Replaced all the previopus functions and variables regarding menus and game state with new one which was being worked on a local copy of the capstone
    Tonight, Ayeman worked through the menus for each game state, such as start menu, settings menu with character display, pause menu, and end game menu. The current problem with menus are, not all characters are showing up in the settings menu, and the player that lost in the tag isn't showing up in the end game menu. Logging out for the night, probably will be fixed by tomorrow.

--- Capstone/sketch.js
+++ Capstone/sketch.js
@@ -6,7 +6,7 @@
 let players = [];
 let map;
 let tagTime = 60; //Time befor a tag acurs in frames
-let time = 100 * 60; // The amount of time in frames that the game is played
+let time = 100 * 1; // The amount of time in frames that the game is played
 let numPlayers = 1; // starts from 0
 let playerTaged;
 let platformImg = [];
@@ -15,14 +15,12 @@ let platform = [];
 let t; //time in seconds
 
 // Ayeman Islam Global Variable
-let menu = [];
-let pauseMenu = [];
-let endGameMenu = [];
 let gameState = 0;// 0 = start, 1 = game is started, 2 = pause menu, 3 = end/over
 
 let play, settings, continueGame, end;
 
 let playButton, settingsButton, continueButton, endButton;
+let time60, time90, time120, time180; // Buttons for each time limit for tagging
 
 
 // player imgs 2d arays for animation
@@ -55,17 +53,6 @@ function preload() {
       playerImgs[n][0].push(loadImage("assets/Map-" + numMap + "/Player (" + n + ")/idle/idle (" + i + ").png"));
     }
   }
-
-  // Ayeman's Gamestate setup
-  play = loadImage("assets/menu1/Play Button.png");
-  settings = loadImage("assets/menu1/Settings Button.png");
-  continueGame = loadImage("assets/menu1/Continue Button.png");
-  end = loadImage("assets/menu1/Quit Button.png");
-
-  menu.push(play, settings);
-  pauseMenu.push(continueGame, settings, end);
-  endGameMenu.push(end);
-
 }
 
 function setup() {
@@ -89,84 +76,167 @@ function setup() {
   playButton = createButton('Play');
   playButton.size(150, 50);
   playButton.style('font-size', '30px');
+  playButton.mousePressed(clickPlay); // Link to our new function
 
   settingsButton = createButton('Settings');
   settingsButton.size(150, 50);
   settingsButton.style('font-size', '30px');
+  settingsButton.mousePressed(openSettings);
+
 
   continueButton = createButton('Continue');
   continueButton.size(150, 50);
   continueButton.style('font-size', '30px');
+  continueButton.mousePressed(clickContinue); // Link to our new function
 
   endButton = createButton('End');
   endButton.size(150, 50);
   endButton.style('font-size', '30px');
+  endButton.mousePressed(clickEnd); // Link to our new function
+
+
+
+  // Ayeman's Time Selection Buttons
+  time60 = createButton('60s');
+  time60.mousePressed(selectTime);
+  time60.size(150, 50);
+  time60.style('font-size', '30px');
+  time60.timeValue = 60;
+
+  time90 = createButton('90s');
+  time90.mousePressed(selectTime);
+  time90.size(150, 50);
+  time90.style('font-size', '30px');
+  time90.timeValue = 90;
+
+  time120 = createButton('120s');
+  time120.mousePressed(selectTime);
+  time120.size(150, 50);
+  time120.style('font-size', '30px');
+  time120.timeValue = 120;
+
+  time180 = createButton('180s');
+  time180.mousePressed(selectTime);
+  time180.size(150, 50);
+  time180.style('font-size', '30px');
+  time180.timeValue = 180;
 }
 
 function draw() {
-  timer();
-  background(220);
-  startMenu();
+  // Pause game logic (80 is the keycode for 'P' refering to pause)
+  if (gameState === 1 && keyIsDown(80)) {
+    gameState = 2;
+  }
 
-  if (keyPressed === 80) gameState = 2;
+  // Drawing the screen based on the gameState
   if (gameState === 0) {
     startMenu();
   }
-  if (gameState === 1) {
+  else if (gameState === 1) {
+    timer();
     gameStarted();
-    if (time) tag();
+    if (time > 0) tag();
+    // displaying the timer
     if (!(time % 60)) {
       t = time / 60
       if (t < 0) t = 0;
     }
+    fill(0);
     text(t, width / 2, 100);
   }
-  if (gameState === 2) {
-    pauseGame();
-  }
-  // else {
-  //   endGameMenu();
-  // }
+  else if (gameState === 2) pauseGame();
+  else if (gameState === 3) endScreen();
 
+
+  // Checks buttons every frame 
   hideOrShowButtons();
 }
-function hideOrShowButtons() {
-  playButton.hide();
-  settingsButton.hide();
-  continueButton.hide();
-  endButton.hide();
 
+function selectTime() { // Ayeman's code
+  // 'this' refers to the button that was just clicked
+  time = this.timeValue * 60;
+  gameState = 0; // Go back to the main menu
+}
+
+function clickPlay() { // Ayeman's code
+  gameState = 1; // Switch to game
+}
+
+function clickContinue() { // Ayeman's code
+  gameState = 1; // Switch back to game from pause
+}
+
+function clickEnd() { // Ayeman's code
+  gameState = 0; // Go back to start menu
+  // Optional: Reset game variables here if you want the game to start fresh
+  time = 100 * 60;
+  tagTime = 60;
+}
+
+function hideTimeButtons() { // Ayeman's code
+  // Hides all the time buttons
+  time60.hide();
+  time90.hide();
+  time120.hide();
+  time180.hide();
+}
+
+function hideOrShowButtons() { // Ayeman's code
+  // 1. Start Menu
   if (gameState === 0) {
     playButton.show();
-    playButton.position(width / 2, height / 2 - 50);
+    playButton.position(width / 2 - 75, height / 2 + 50);
     settingsButton.show();
-    settingsButton.position(width / 2, height / 2);
-    startMenu();
+    settingsButton.position(width / 2 - 75, height / 2 + 120);
+
+    continueButton.hide();
+    endButton.hide();
+    hideTimeButtons(); // Helper to hide time buttons
   }
-  if (gameState === 1) {
+  // 2. Playing Game
+  else if (gameState === 1) {
     playButton.hide();
     settingsButton.hide();
     continueButton.hide();
     endButton.hide();
+    hideTimeButtons();
   }
-  if (gameState === 2) {
-    settingsButton.show();
-    settingsButton.position(width / 2, height / 2);
+  // 3. Pause Menu
+  else if (gameState === 2) {
+    playButton.hide();
+    settingsButton.hide();
     continueButton.show();
-    continueButton.position(width / 2, height / 2 - 50);
+    continueButton.position(width / 2 - 75, height / 2);
     endButton.show();
-    endButton.position(width / 2, height / 2 + 50);
-    pauseGame()
+    endButton.position(width / 2 - 75, height / 2 + 50);
+    hideTimeButtons();
   }
-  if (gameState === 3) {
-    settingsButton.show();
-    settingsButton.position(width / 2, height / 2);
-    endButton.show()
-    endButton.position(width / 2, height / 2 + 50);
+  // 4. End Screen
+  else if (gameState === 3) {
+    playButton.hide();
+    settingsButton.hide();
+    continueButton.hide();
+    endButton.show();
+    endButton.position(width / 2 - 75, height / 2 + 50);
+    hideTimeButtons();
+  }
+  // 5. Settings Menu
+  else if (gameState === 4) {
+    playButton.hide();
+    settingsButton.hide();
+    continueButton.hide();
+    endButton.hide();
+
+    // Show time buttons
+    time60.show();  time60.position(width / 2 - 500, height / 2 - 130);
+    time90.show();  time90.position(width / 2 - 200, height / 2 - 130);
+    time120.show(); time120.position(width / 2 + 110, height / 2 - 130);
+    time180.show(); time180.position(width / 2 + 410, height / 2 - 130);
   }
 }
 
-function gameStarted() {
+
+function gameStarted() { // Ayeman's code
   // this function will run all things that the game tag needs so like player timer and what not
   textSize(50); fill(0);
 
@@ -176,32 +246,75 @@ function gameStarted() {
   }
 }
 
-function startMenu() {
+function startMenu() { // Ayeman's code
   // For the start menu
   // a page to the controls a option to chous witch charcter player 1 ,2 .. whant to be and how many players, buffs
   // no buffs. Timer options to chous what time you whoud like 60 s or 120.
 
-  background(250);
+  fill(200, 150, 150, 75);
+  rect(0, 0, width, height); // creating background
 
-  // image(menu[0], 400, 400);
-  // image(menu[1], 400, 450); 
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textFont('Helevetica');
+  textSize(180);
+  textStyle(BOLD, ITALIC);
+  text("TAG GAME", width / 2, height / 2 - 150);
 
-  playButton.mousePressed(gameStarted);
+}
 
-  // if(gameState === 0){
-  //   playButton.hide();
-  //   settingsButton.hide();
-  //   continueButton.hide();
-  //   endButton.hide();
-  // }
-  // else if(gameState === 2 || gameState === 3){
-  //   playButton.hide();
-  //   settingsButton.show();
-  // }
-  // else{
-  //   playButton.show();
-  //   settingsButton.show();
-  // }
+function openSettings() {
+  // for the settings menu
+  // Allows the user to change the time limit 
+  // And check on the controls for each player
+
+  fill(50, 150, 185);
+  rect(0, 0, width, height); // creating background
+
+  fill(255);
+  textStyle(ITALIC);
+  textFont('Arial'); textAlign(CENTER, CENTER); textSize(45);
+  text("SELECT TIME LIMIT", width / 2, height / 2 - 200);
+
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textFont('Helevetica'); textSize(180); textStyle(BOLD, ITALIC);
+  text("SETTINGS MENU", width / 2, height / 2 - 350); // title screen
+  gameState = 4; // switches to settings
+
+  // Player Preview 
+  for (let n = 0; n < 4; n++) {
+  if (playerImgs[n] && playerImgs[n][0] && playerImgs[n][0][0]) {
+    let idleImg = playerImgs[n][0][0];
+    
+    // 1. Grid Math
+    let col = n % 2;        // 0, 1, 0, 1 (Switches between left and right)
+    let row = floor(n / 2); // 0, 0, 1, 1 (Stays top row then moves to bottom)
+    
+    // 2. Calculate positions
+    // Adjust these values to shift the whole grid around
+    let startX = width / 2 + 250; 
+    let startY = height / 2 + 45;
+    
+    let xPos = startX + (col * 300); // 300px spacing between columns
+    let yPos = startY + (row * 300); // 300px spacing between rows
+    
+    // 3. Draw image and label
+    image(idleImg, xPos, yPos, 100, 150);
+    textSize(35);
+    textAlign(CENTER, CENTER);
+    text("PLAYER " + (n + 1), xPos + 50, yPos + 180);
+  }
+}
+  
+
+  //Controls Display
+  fill(205,255,255);
+  textAlign(CENTER, CENTER); textSize(35); textFont('Arial');
+  text("PLAYER 1: ⬅️/➡️ to Move Left & Right, ⬆️ to Jump", width / 2 - 470, height / 2 + 100);
+  text("PLAYER 2: A/D to Move Left & Right, W to Jump", width / 2 - 500, height / 2 + 200);
+  text("PLAYER 3: J/L to Move Left & Right, I to Jump", width / 2 - 515, height / 2 + 300);
+  text("PLAYER 4: F/H to Move Left & Right, T to Jump", width / 2 - 505, height / 2 + 400);
 
 }
 
@@ -210,35 +323,54 @@ function pauseGame() {
   // alowes user to hit a button like p to open the pause menue, 
   // stops all movemnt and then you can change either resume end or restart, see the player movement keys
 
-  // background(25);
-  // if(gameState === 0){
-  //   playButton.hide();
-  //   settingsButton.hide();
-  // }
-  // else if(gameState === 2 || gameState === 3){
-  //   playButton.hide();
-  // }
-  // else{
-  //   playButton.hide();
-  //   settingsButton.hide();
-  // }
-  endButton.mousePressed(startMenu);
+  fill(200, 0, 150, 10);
+  rect(0, 0, width, height); // creating background
 
-  // image(pauseMenu[0], 400, 400);
-  // image(pauseMenu[1], 400, 450);
-  // image(pauseMenu[2], 400, 500);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(80);
+  text("PAUSED", width / 2, height / 2 - 150); // title screen
 
 }
 
 function endScreen() {
   // The end screen for who won
   // overlays who lost on a side and who one on a podioum
+  
+  fill(50, 100, 150, 55);
+  rect(0, 0, width, height); // creating background
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(80);
+  text("GAME OVER", width / 2, height / 2 - 150); // title screen
+
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].isTaged === 1) {
+      
+      // Display the loser in the center
+      if (playerImgs[i] && playerImgs[i][0] && playerImgs[i][0][0]) {
+        let loserImg = playerImgs[i][0][0];
+        image(loserImg, width / 2 - 75, height / 2 - 100, 150, 225);
+      }
+
+      // Display the loser's name
+      textSize(50);
+      fill(255, 50, 50);
+      text("PLAYER " + (i + 1) + " LOST!", width / 2, height / 2 + 150);
+      
+      // Stop the game loop for everyone
+      players[i].gameOver = 0;
+    }
+  }
+
+
   for (let p in players) {
     players[p].gameEnd();
-
   }
-  gameState = 4; // game is over
+  gameState = 3; // game is over
   endButton.position(400, 400);
+
+
 }
 
 function timer() {
@@ -250,17 +382,15 @@ function timer() {
 
   if (time <= 0) {
     // if the game time is over then the game is over
+    gameState = 3; //sets the game set to End screen
     endScreen();
   }
-
   else {
     if (!(time % 60)) {
       // prints the time every 1 sec so 60 frames
       print(time / 60);
     }
-
   }
-
 }
 
 function powerUps() {
@@ -525,7 +655,7 @@ class Player {
       else {
         print("HAHAH I WON, GG")
       }
-      this.gameOver = 0;
+      //this.gameOver = 0;
     }
   }
 } 
